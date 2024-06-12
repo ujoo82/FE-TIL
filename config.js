d776e136c89b01e6420acc73335025aeee83797c
Yuju_Jang <ujoo2802@gmail.com>
2024-06-12 19:19:21 +0900

    내용 수정

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // 기본 설정 파일
 // 여기서 사용자 입장에서 꼭 입력해야 하는 정보를 의사결정해서 개발에 반영할 필요가 있습니다.
 const siteConfig = {
-  username: "", // GitHub 사용자 이름
-  repositoryName: "", // GitHub 저장소 이름
+  username: "ur-zoo", // GitHub 사용자 이름
+  repositoryName: "FE-TIL", // GitHub 저장소 이름
   mainColor: "#3498db", // 사이트의 주 색상
   textColor: "#333333", // 기본 텍스트 색상
-  blogTitle: "", // 블로그 제목
+  blogTitle: "Ur Zoo", // 블로그 제목
 };
 
 // 여러명의 저자가 글을 쓸 경우 프로필 설정, default는 0번째 사용자
@@ -13,9 +13,9 @@ const siteConfig = {
 const users = [
   {
     id: 0, // default author
-    username: "licat",
-    company: "weniv",
-    position: "CEO",
+    username: "yuju",
+    company: "",
+    position: "",
     img: "img/user/profile-licat.png",
   },
 ];
